eab1b358725de2f886a6bc8110fd271f093d2456
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2022-09-22 12:59:06 -0700

    Finished hw4

--- Week4/hw4_TaylorHancock.cpp
+++ Week4/hw4_TaylorHancock.cpp
@@ -1,8 +1,8 @@
 /*****************************************
  * Author:    Taylor Hancock
- * Purpose:   
+ * Purpose:   Practice working with classes and their basic operations
  * Version:   1.0 09/20/2022
- * Resources: 
+ * Resources: N/A
  ******************************************/
 
 #include <iostream>
@@ -12,31 +12,30 @@ using namespace std;
 
 class Car {
     private:
-        string make;
-        double milesPerGallon;
-        double fuel;
-        double fuelCapacity;
-        int odometer;
+        string make;            // the car's make
+        double milesPerGallon;  // the car's miles per gallon
+        double fuel;            // the amount of fuel in the car's tank
+        double fuelCapacity;    // the car's fuel capacity
+        int odometer;           // the reading on the car's odometer (miles driven)
     public:
         string getMake() {return make;}
         double getMilesPerGallon() {return milesPerGallon;}
         double getFuel() {return fuel;}
         double getFuelCapacity() {return fuelCapacity;}
-        void showCar();
-        void fuelUp(double fuelAmount);
-        void drive();
-        void drive(int distance);
-        void setCar(string make, double milesPerGallon, double fuelCapacity, double fuel, int odometer);
+        void showCar();                 // prints info about the car
+        void fuelUp(double fuelAmount); // fuels the car up by some amount (or can siphon fuel from the tank)
+        void drive();                   // has the car drive some distance between 0-100 miles
+        void drive(int distance);       // drives the car some number of miles
+        void setCar(string make, double milesPerGallon, double fuelCapacity, double fuel, int odometer); // initializes the car (should probably be a constructor, but the homework doesn't say to do that)
 };
 
 int main() {
     srand(time(NULL));   // initialize srand
 
+    // setup car
     Car c;
     c.setCar("Honda", 26.2, 14.0, 12.8, 3300);
     int userInput = 0;
-    c.showCar();
-    return 0;
 
     do {
         c.showCar();
@@ -46,9 +45,9 @@ int main() {
             cin.clear();
             cin.ignore(INT_MAX, '\n');
             cout << "Error: Invalid Input! Please input a valid integer in the range 1-4!" << endl;
-        } else if(userInput == 1) {
+        } else if(userInput == 1) { // joy ride
             c.drive();
-        } else if(userInput == 2) {
+        } else if(userInput == 2) { // drive a distance
             cout << "How far do you drive? ";
             int dist;
             cin >> dist;
@@ -59,7 +58,7 @@ int main() {
                 cin >> dist;
             }
             c.drive(dist);
-        } else if(userInput == 3) {
+        } else if(userInput == 3) { // add fuel
             cout << "How much fuel do you add? ";
             int fuelAmt;
             cin >> fuelAmt;
@@ -71,7 +70,11 @@ int main() {
             }
             c.fuelUp(fuelAmt);
         }
-    } while (userInput != 4);
+    } while (userInput != 4); // exit loop
+
+    cout << "You are done driving for today! Goodbye." << endl;
+
+    return 0;
 }
 
 void Car::showCar() {
@@ -85,11 +88,20 @@ void Car::showCar() {
 }
 
 void Car::fuelUp(double fuelAmount) {
-    cout << "You fill your tank with " << fuelAmount << " gallons of fuel." << endl;
-    if(fuelAmount + fuel >= fuelCapacity) { // too much fuel
+    if(fuelAmount < 0) { // if negative fuel
+        cout << "You...you input a negative...uhh...sure, I guess we'll siphon " << fuelAmount * -1 << " gallons of fuel out of your tank." << endl;
+    } else if(fuelAmount == 0) { // if zero fuel
+        cout << "...this just entails doing nothing, but sure, you add a whopping zero gallons of fuel to your tank. Great job." << endl;
+    } else { // if fuel
+        cout << "You fill your tank with " << fuelAmount << " gallons of fuel." << endl;
+    }
+    if(fuelAmount + fuel > fuelCapacity) { // too much fuel
         fuel = fuelCapacity;
-        cout << "Sadly, your tank does not have room, and you spill " << fuelCapacity - (fuelAmount + fuel) << " gallons of fuel on the ground." << endl
-             << "This is a fire hazard, and you should probably do something about it, but sadly there is no option to in the menu, so you carry on." << endl;
+        cout << "Sadly, your tank does not have room, and you spill " << (fuelAmount + fuel) - fuelCapacity << " gallons of fuel on the ground." << endl
+            << "This is a fire hazard, and you should probably do something about it, but sadly there is no option to in the menu, so you carry on." << endl;
+    } else if(fuelAmount + fuel < 0) { // if the amount drops the tank below 0 (for negatives "siphoning")
+        fuel = 0;
+        cout << "You run out of fuel before you can siphon " << fuelAmount * -1 << " gallons out of your tank, but you now have zero gallons of fuel in your tank." << endl;
     } else { // reasonable amount of fuel
         fuel += fuelAmount;
         cout << "You now have " << fuel << " gallons of fuel." << endl;
@@ -97,23 +109,30 @@ void Car::fuelUp(double fuelAmount) {
 }
 
 void Car::drive() {
-    drive(rand() % 101);
+    drive(rand() % 101); // drive a random distance from 0 to 100
 }
 
 void Car::drive(int distance) {
-    cout << "You drive " << distance << "miles." << endl;
-    if(fuel == 0) { // if no fuel
-        cout << "You have no gas, so you get out, and push your car to the destination. You are now very tired. Why on earth did you do that?" << endl;
-    } else if(distance / milesPerGallon > fuel) { // if too far for current gas
-        fuel = 0;
-        odometer += fuel * milesPerGallon;
-        cout << "You run out of gas " << fuel * milesPerGallon << "miles short of your destination. You get out, and push your car to the destination. You are now very tired." << endl;
-    } else { // if enough fuel
-        fuel -= distance / milesPerGallon;
-        odometer += distance;
+    if(distance < 0) { // if negative distance
+        cout << "You seriously expect to drive a negative distance? Please, input a positive distance to travel, we don't want to violate causality here." << endl;
+    } else if(distance == 0) { // if 0 distance
+        cout << "You...you want to drive no distance? Why...why did you even put that in? Do you want me to congratulate you? Wow, you've driven 0 miles! *sigh*" << endl;
+    } else { // if positive, print distance
+        cout << "You drive " << distance << " miles." << endl;
+        if(fuel == 0) { // if no fuel
+            cout << "You have no gas, so you get out, and push your car to the destination. You are now very tired. Why on earth did you do that?" << endl;
+        } else if(distance / milesPerGallon > fuel) { // if too far for current gas
+            odometer += fuel * milesPerGallon; // your odometer is electric and doesn't run when the car is off (mental reason for why you can push the car but not rack up driven miles)
+            cout << "You run out of gas " << fuel * milesPerGallon << "miles short of your destination. You get out, and push your car to the destination. You are now very tired." << endl;
+            fuel = 0;
+        } else { // if enough fuel
+            fuel -= distance / milesPerGallon;
+            odometer += distance;
+        }
     }
 }
 
+// we'll just assume set car is being used safely in this instance, as the inputs are being checked when users input (I know it's not best practices, but I figure it should be enough for this assignment)
 void Car::setCar(string make, double milesPerGallon, double fuelCapacity, double fuel, int odometer) {
     this->make = make;
     this->milesPerGallon = milesPerGallon;
