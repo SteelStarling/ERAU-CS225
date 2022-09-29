f110df754800658abbc246c64e2adb5f344d563e
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2022-09-29 13:02:58 -0700

    finished InClass Exercise 9/28

--- Week5/Cat.cpp
+++ Week5/Cat.cpp
@@ -1,3 +1,11 @@
+/**************************************************
+ * File:      Cat.cpp
+ * Author:    Taylor Hancock (with 95% of the core stuff provided by Dr. Marriott)
+ * Purpose:   To get practice with the "this" keyword
+ * Version:   1.0 Sep 28, 2022
+ * Resources: Lecture notes
+ **************************************************/
+
 #include <string>
 #include <iostream>
 using namespace std;
@@ -16,11 +24,30 @@ public:
 		cout << " name = " << name << endl;
 	}
 
-	void greeting(string saying) { 
+	Pet();
+
+	virtual void greeting(string saying) { 
 		cout << name << " (" << weight << ") pounds";
 	    cout<< " says " << saying << endl;
 	}
 };
+
+Pet::Pet() {
+	cout << "Input the weight: ";
+	cin >> weight;
+	while(!cin || weight <= 0) {
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "ERROR: Invalid weight! Must be float and greater than 0!" << endl;
+		cout << "Input the weight: ";
+		cin >> weight;
+	}
+
+	cin.ignore(INT_MAX, '\n'); // ignore before searching (prevents weird bugs)
+
+	cout << "Input the name: ";
+	getline(cin, name);
+}
 
 class Cat : public Pet {
 public:
@@ -29,6 +56,11 @@ public:
 	//an initialization list.
 	Cat(float weight, string name):Pet(weight, name) {  }
 	Cat(string name) : Pet(10, name) { }
+	Cat() : Pet() { }
+
+	void greeting(string saying) {
+		cout << "The cat says " << saying << endl;
+	}
 	
 };
 
@@ -39,14 +71,22 @@ int main() {
 	theInternationalStandardCat.greeting("Boy oh boy, am I a standard cat by international treaty!");
 	Pet* mystery;
 
-	// no data checking for now
 	string type;
 	cout << "Would you like a Cat or a Pet (answer in lowercase)? ";
 	cin >> type;
-	if(type == "pet") {
-		
-	} else {
-		
+	// data checking
+	while(type != "pet" && type != "cat") {
+		cout << "ERROR: Invalid type!" << endl;
+		cout << "Would you like a Cat or a Pet (answer in lowercase)? ";
+		cin >> type;
 	}
+
+	if(type == "pet") {
+		mystery = new Pet();
+	} else if(type == "cat") {
+		mystery = new Cat();
+	}
+
+	mystery->greeting("Aloha");
 	return 0;
 }
